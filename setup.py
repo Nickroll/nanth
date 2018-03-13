4e0eef2d471c3741704555dbb1ba38fb5bee5a3d
Nicholas Roller <rick.nickroller@gmail.com>
2018-03-13 14:25:35 -0400

    Updating __init__ and setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='nanth',
+	version='0.01',
+	description='Personal tools for use in scientific analysis',
+	url='https://github.com/Nickroll/nanth',
+	author='Nick Roller',
+	author_email='rick.nickroller@gmail.com',
+	license='MIT',
+	packages=['nanth'],
+	zip_safe=False)
+
